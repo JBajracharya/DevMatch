1b65d1124eb2d50258224dc3af0c8927973955e9
jbajracharya <jitendra.bajracharya01@outlook.com>
2020-03-09 23:16:03 -0700

    added sign in and sign out ability

--- app/src/main/java/com/finalProject/devmatch/MainActivity.java
+++ app/src/main/java/com/finalProject/devmatch/MainActivity.java
@@ -1,18 +1,26 @@
 package com.finalProject.devmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserState;
+import com.amazonaws.mobile.client.UserStateDetails;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    static String TAG = "mainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +37,61 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // initializing aws authentication
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+
+                    @Override
+                    public void onResult(UserStateDetails userStateDetails) {
+                        Log.i("INIT", "onResult: " + userStateDetails.getUserState());
+                        if(userStateDetails.getUserState().equals(UserState.SIGNED_OUT)) {
+
+                            //        Drop-in pre build auth
+                            // 'this' refers to the current active activity
+                            AWSMobileClient.getInstance().showSignIn(MainActivity.this, SignInUIOptions.builder()
+                                            .nextActivity(MainActivity.class)
+                                            .build(),
+                                    new Callback<UserStateDetails>() {
+                                        @Override
+                                        public void onResult(UserStateDetails result) {
+                                            Log.d(TAG, "onResult: " + result.getUserState());
+                                            switch (result.getUserState()){
+                                                case SIGNED_IN:
+                                                    Log.i("INIT", "logged in!");
+                                                    break;
+                                                case SIGNED_OUT:
+                                                    Log.i(TAG, "onResult: User did not choose to sign-in");
+                                                    break;
+                                                default:
+                                                    AWSMobileClient.getInstance().signOut();
+                                                    break;
+                                            }
+
+                                        }
+
+                                        @Override
+                                        public void onError(Exception e) {
+                                            Log.e(TAG, "onError: ", e);
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("INIT", "Initialization error.", e);
+                    }
+                }
+        );
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
@@ -47,6 +104,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+                return true;
+        }
+
+        if(id == R.id.action_signOut) {
+            //    method to log out user and send to log in page :::::::::::::::::::
+            AWSMobileClient.getInstance().signOut();
+            Intent goToMainPage = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(goToMainPage);
             return true;
         }
 
