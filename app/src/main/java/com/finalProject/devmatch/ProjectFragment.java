247e4e82ffd30c028742433f8ab2e6f22efafc1b
tcoots88 <travis.coots@yahoo.com>
2020-03-10 14:56:17 -0700

    nav bar now will navigate to pages appropriately

---
+++ app/src/main/java/com/finalProject/devmatch/ProjectFragment.java
@@ -0,0 +1,18 @@
+package com.finalProject.devmatch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProjectFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_search_for_projects, container, false);
+    }
+}
