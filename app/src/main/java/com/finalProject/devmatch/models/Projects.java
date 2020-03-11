1ac460771abe315ff76c6899e6dd867f4c6ff06c
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2020-03-11 15:24:54 -0700

    updated project class

--- app/src/main/java/com/finalProject/devmatch/models/Projects.java
+++ app/src/main/java/com/finalProject/devmatch/models/Projects.java
@@ -1,27 +1,193 @@
 package com.finalProject.devmatch.models;
 
 
+import android.os.Environment;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import okhttp3.internal.platform.Platform;
 
 public class Projects {
 
 
     private String name;
     private String description;
-    private String position;
+    private Language language;
+    private Database dataBase;
+    private Environment environment;
+
+
+
+    private Platform platform;
     private String date;
     private String link;
 
     private ArrayList<Developer> developers;
 
 
-    public Projects(String name, String description, String position, String date, String link) {
+    public Projects(String name, String description, String date, String link) {
         this.name = name;
         this.description = description;
-        this.position = position;
         this.date = date;
         this.link = link;
     }
+
+    private enum Language {
+
+        java,
+        javascript,
+        python,
+        csharp,
+        cplusplus,
+        ruby,
+        dotNet,
+        sql,
+        html,
+        css,
+
+    }
+
+
+    public void setLanguage(String language){
+        switch (language){
+            case "Java":
+                this.language =Language.java;
+                break;
+            case "JavaScript":
+                this.language = Language.javascript;
+                break;
+            case "Python":
+                this.language = Language.python;
+                break;
+            case "Csharp":
+                this.language = Language.csharp;
+                break;
+            case "Cplusplus":
+                this.language = Language.cplusplus;
+                break;
+            case "Ruby":
+                this.language = Language.ruby;
+                break;
+            case "dotNet":
+                this.language = Language.dotNet;
+                break;
+            case "sql":
+                this.language = Language.sql;
+                break;
+            case "html":
+                this.language = Language.html;
+                break;
+            case "css":
+                this.language = Language.css;
+                break;
+
+
+        }
+
+    }
+
+    private enum Database {
+
+        postgresql,
+        mysql,
+        mongoDB,
+        dynamoDB,
+
+
+    }
+
+    public void setDatabase(String database){
+        switch (database){
+            case "Postgresql":
+                this.dataBase =Database.postgresql;
+                break;
+            case "Mysql":
+                this.dataBase =Database.mysql;
+                break;
+            case "MongoDB":
+                this.dataBase =Database.mongoDB;
+                break;
+            case "DynamoDB":
+                this.dataBase =Database.dynamoDB;
+                break;
+
+
+        }
+
+    }
+    private enum Environment {
+        aws,
+        heroku,
+        firebase,
+        azure,
+
+
+
+
+    }
+
+    public void setEnvironment(String environment){
+        switch (environment){
+            case "AWS":
+                this.environment =Environment.aws;
+                break;
+            case "Heroku":
+                this.environment =Environment.heroku;
+                break;
+            case "Firebase":
+                this.environment =Environment.firebase;
+                break;
+            case "Azure":
+                this.environment =Environment.azure;
+                break;
+
+
+        }
+
+    }
+
+    private enum Platform {
+         iOS,
+         android,
+         linux,
+        web,
+        react,
+
+
+
+
+    }
+
+    public void setPlatform(String platform){
+        switch (platform){
+            case "IOS":
+                this.platform =Platform.iOS;
+                break;
+            case "Android":
+                this.platform =Platform.android;
+                break;
+            case "Linux":
+                this.platform =Platform.linux;
+                break;
+            case "Web":
+                this.platform =Platform.web;
+                break;
+
+            case "React":
+                this.platform =Platform.react;
+                break;
+
+
+        }
+
+    }
+
+
+
+
+
+
 
     public Projects(){
 
@@ -36,9 +202,6 @@ public class Projects {
         return description;
     }
 
-    public String getPosition() {
-        return position;
-    }
 
     public String getDate() {
         return date;
@@ -56,9 +219,6 @@ public class Projects {
         this.description = description;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
-    }
 
     public void setDate(String date) {
         this.date = date;
@@ -67,4 +227,44 @@ public class Projects {
     public void setLink(String link) {
         this.link = link;
     }
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public ArrayList<Developer> getDevelopers() {
+        return developers;
+    }
+
+    public void setDevelopers(ArrayList<Developer> developers) {
+        this.developers = developers;
+    }
+
+    public Database getDataBase() {
+        return dataBase;
+    }
+
+    public void setDataBase(Database dataBase) {
+        this.dataBase = dataBase;
+    }
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Platform platform) {
+        this.platform = platform;
+    }
+
+
 }
