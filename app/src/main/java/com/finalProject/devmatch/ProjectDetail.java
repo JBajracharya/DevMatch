04097a168227f04ab0e762212dfdeb3a26836a41
Shane Gable <gableshane@gmail.com>
2020-03-12 16:32:00 -0700

    request and approve

--- app/src/main/java/com/finalProject/devmatch/ProjectDetail.java
+++ app/src/main/java/com/finalProject/devmatch/ProjectDetail.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amazonaws.amplify.generated.graphql.CreateProjectMutation;
 import com.amazonaws.amplify.generated.graphql.ListProjectsQuery;
+import com.amazonaws.amplify.generated.graphql.UpdateDeveloperMutation;
+import com.amazonaws.amplify.generated.graphql.UpdateProjectMutation;
+import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
 import com.apollographql.apollo.GraphQLCall;
@@ -20,6 +27,10 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import type.CreateProjectInput;
+import type.UpdateDeveloperInput;
+import type.UpdateProjectInput;
+
 public class ProjectDetail extends AppCompatActivity {
 
     private AWSAppSyncClient mAWSAppSyncClient;
@@ -30,6 +41,10 @@ public class ProjectDetail extends AppCompatActivity {
     TextView name;
     TextView description;
     TextView link;
+    Button apply;
+
+    TextView requester;
+    Button approve;
 
 
     @Override
@@ -44,9 +59,23 @@ public class ProjectDetail extends AppCompatActivity {
         name = findViewById(R.id.textView4);
         description = findViewById(R.id.textView8);
         link = findViewById(R.id.textView10);
+        apply = findViewById(R.id.apply);
+        requester = findViewById(R.id.requester);
+        approve = findViewById(R.id.approve);
+
+        requester.setVisibility(View.INVISIBLE);
+        approve.setVisibility(View.INVISIBLE);
 
         id = getIntent().getStringExtra("mID");
         getProjects();
+
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runProjectsUpdateMutation();
+                Toast.makeText(getApplicationContext(),"You have requested to be part of this project",Toast.LENGTH_SHORT);
+            }
+        });
     }
     public void getProjects(){
 
@@ -74,6 +103,13 @@ public class ProjectDetail extends AppCompatActivity {
                             project.setDate(p.date());
                             project.setLink(p.link());
                             projectStuff();
+
+                            if(AWSMobileClient.getInstance().getUsername() == project.getOwner() && !project.getDevRequests().isEmpty()){
+                                requester.setText(project.getDevRequests().get(0));
+                                requester.setVisibility(View.VISIBLE);
+                                approve.setVisibility(View.VISIBLE);
+                                runProjectsUpdateMutationForApproval();
+                            }
                         }
                     }
                 }
@@ -83,6 +119,39 @@ public class ProjectDetail extends AppCompatActivity {
                     Log.i(TAG,"Failure");
                 }
             };
+    public void runProjectsUpdateMutation() {
+        UpdateProjectInput updateProjectInput = UpdateProjectInput.builder()
+                .id(id)
+                .devRequests(AWSMobileClient.getInstance().getUsername())
+                .build();
+
+        mAWSAppSyncClient.mutate(UpdateProjectMutation.builder().input(updateProjectInput).build())
+                .enqueue(mutationCallbacky);
+    }
+    public void runProjectsUpdateMutationForApproval() {
+        UpdateProjectInput updateProjectInput = UpdateProjectInput.builder()
+                .id(id)
+                .devRequests(null)
+                .developers(project.getDevRequests().get(0))
+                .build();
+
+        mAWSAppSyncClient.mutate(UpdateProjectMutation.builder().input(updateProjectInput).build())
+                .enqueue(mutationCallbacky);
+    }
+
+    private GraphQLCall.Callback<UpdateProjectMutation.Data> mutationCallbacky = new GraphQLCall.Callback<UpdateProjectMutation.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<UpdateProjectMutation.Data> response) {
+            Log.i(TAG, response.data().toString());
+            Log.i(TAG,"SUCCESS");
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e("Error", e.toString());
+        }
+
+    };
     public void projectStuff(){
         name.setText(project.getName());
         description.setText(project.getDescription());
