73640995a68b711a8cad8397f983b1155671bcc7
Shane Gable <gableshane@gmail.com>
2020-03-13 05:52:48 -0700

    save

--- app/src/main/java/com/finalProject/devmatch/Profile.java
+++ app/src/main/java/com/finalProject/devmatch/Profile.java
@@ -15,21 +15,8 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        final TextView profileTitle=findViewById((R.id.profiletitle));
-        final TextView profileName=findViewById((R.id.profilename));
-        final TextView profileEmail=findViewById((R.id.profileemail));
-        final TextView profileGithub=findViewById((R.id.usergithub));
-        final TextView profileJobType=findViewById((R.id.profiletype));
-        final Button createProfile = findViewById(R.id.createProfile);
-        final Button editProfile = findViewById(R.id.editProfile);
 
-        createProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Profile.this, CreateProfile.class);
-                Profile.this.startActivity(i);
-            }
-        });
+
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
